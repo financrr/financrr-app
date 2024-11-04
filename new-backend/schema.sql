d6a65e64ec7fbc2b912153405081bca3c1cce824
Timon Klinkert <timon.klinkert@denux.dev>
2024-11-04 20:46:21 +0100

    added transaction tables

--- new-backend/schema.sql
+++ new-backend/schema.sql
@@ -23,7 +23,7 @@ CREATE TABLE sessions
     api_key     TEXT                                             NOT NULL UNIQUE,
     name        TEXT,
     description TEXT,
-    platofrm    TEXT,
+    platform    TEXT,
     created_at  timestamp with time zone                         NOT NULL,
     updated_at  timestamp with time zone                         NOT NULL
 );
@@ -72,15 +72,15 @@ CREATE TABLE linked_back_account_user_permissions
 CREATE TABLE bank_accounts
 (
     id                     BIGINT PRIMARY KEY,
-    name                   TEXT                            NOT NULL,
+    name                   TEXT                              NOT NULL,
     description            TEXT,
     iban                   TEXT UNIQUE,
-    balance                BIGINT                          NOT NULL DEFAULT 0,
-    original_balance       BIGINT                          NOT NULL DEFAULT 0,
-    currency_id            BIGINT REFERENCES Currency (id) NOT NULL,
-    linked_back_account_id BIGINT                          REFERENCES linked_back_accounts (id) ON DELETE SET NULL,
-    created_at             timestamp with time zone        NOT NULL,
-    updated_at             timestamp with time zone        NOT NULL,
+    balance                BIGINT                            NOT NULL DEFAULT 0,
+    original_balance       BIGINT                            NOT NULL DEFAULT 0,
+    currency_id            BIGINT REFERENCES currencies (id) NOT NULL,
+    linked_back_account_id BIGINT                            REFERENCES linked_back_accounts (id) ON DELETE SET NULL,
+    created_at             timestamp with time zone          NOT NULL,
+    updated_at             timestamp with time zone          NOT NULL
 );
 
 CREATE TABLE bank_account_user_permissions
@@ -92,3 +92,40 @@ CREATE TABLE bank_account_user_permissions
     updated_at      timestamp with time zone                               NOT NULL,
     PRIMARY KEY (user_id, bank_account_id)
 );
+
+CREATE TABLE base_transactions
+(
+    id          BIGINT PRIMARY KEY,
+    source      BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    destination BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    amount      BIGINT                                                                NOT NULL,
+    currency    BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    name        TEXT                                                                  NOT NULL,
+    description TEXT,
+    created_at  timestamp with time zone                                              NOT NULL,
+    updated_at  timestamp with time zone                                              NOT NULL
+);
+
+CREATE TABLE transactions
+(
+    executed_at timestamp with time zone,
+    PRIMARY KEY (id)
+) INHERITS (base_transactions);
+
+CREATE TABLE transaction_templates
+(
+    PRIMARY KEY (id)
+) INHERITS (base_transactions);
+
+CREATE TABLE recurring_transactions
+(
+    recurring_rule   json NOT NULL,
+    last_executed_at timestamp with time zone,
+    PRIMARY KEY (id)
+) INHERITS (base_transactions);
+
+CREATE TABLE scheduled_transactions
+(
+    scheduled_at timestamp with time zone,
+    PRIMARY KEY (id)
+) INHERITS (base_transactions);
