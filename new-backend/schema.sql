8347472a7cf40021ad7cca8f8fc7689850b0b546
Denux <timon.klinkert@denux.dev>
2024-11-20 20:55:07 +0100

    A lot of schema and setup work

--- new-backend/schema.sql
+++ new-backend/schema.sql
@@ -21,6 +21,8 @@ CREATE TABLE users
     updated_at                 timestamp with time zone NOT NULL
 );
 
+CREATE INDEX idx_user_email ON users (email);
+
 CREATE TABLE user_permissions
 (
     user_id     BIGINT REFERENCES "users" (id) ON DELETE CASCADE NOT NULL,
@@ -31,6 +33,9 @@ CREATE TABLE user_permissions
     updated_at  timestamp with time zone                         NOT NULL,
     PRIMARY KEY (user_id, entity_type, entity_id)
 );
+
+CREATE INDEX idx_user_permissions_user_id ON user_permissions (user_id);
+CREATE INDEX idx_user_permissions_entity ON user_permissions (entity_type, entity_id);
 
 CREATE TABLE sessions
 (
@@ -44,16 +49,22 @@ CREATE TABLE sessions
     updated_at       timestamp with time zone                         NOT NULL
 );
 
+CREATE INDEX idx_sessions_user_id ON sessions (user_id);
+CREATE INDEX idx_sessions_api_key ON sessions (api_key);
+
 CREATE TABLE file_attachments
 (
-    id         BIGINT PRIMARY KEY,
-    name       TEXT                     NOT NULL,
-    path       TEXT                     NOT NULL,
-    type       TEXT                     NOT NULL,
-    size       BIGINT                   NOT NULL,
-    created_at timestamp with time zone NOT NULL,
-    updated_at timestamp with time zone NOT NULL
+    id                  BIGINT PRIMARY KEY,
+    globally_accessible BOOLEAN                  NOT NULL,
+    name                TEXT                     NOT NULL,
+    path                TEXT                     NOT NULL,
+    type                TEXT                     NOT NULL,
+    size                BIGINT                   NOT NULL,
+    created_at          timestamp with time zone NOT NULL,
+    updated_at          timestamp with time zone NOT NULL
 );
+
+CREATE INDEX idx_file_attachments_name ON file_attachments (name);
 
 CREATE TABLE currencies
 (
@@ -77,6 +88,9 @@ CREATE TABLE categories
     updated_at timestamp with time zone NOT NULL
 );
 
+CREATE INDEX idx_categories_user_id ON categories (user_id);
+CREATE INDEX idx_categories_parent_id ON categories (parent_id);
+
 CREATE TABLE tags
 (
     id         BIGINT PRIMARY KEY,
@@ -85,6 +99,8 @@ CREATE TABLE tags
     created_at timestamp with time zone                         NOT NULL,
     updated_at timestamp with time zone                         NOT NULL
 );
+
+CREATE INDEX idx_tags_user_id ON tags (user_id);
 
 CREATE TABLE taggings
 (
@@ -95,6 +111,36 @@ CREATE TABLE taggings
     updated_at  timestamp with time zone                      NOT NULL,
     PRIMARY KEY (tag_id, entity_type, entity_id)
 );
+
+CREATE INDEX idx_taggings_tag_id ON taggings (tag_id);
+CREATE INDEX idx_taggings_entity ON taggings (entity_type, entity_id);
+
+-- ############################################################
+-- #                                                          #
+-- #                External Bank Accounts                    #
+-- #                                                          #
+-- ############################################################
+
+CREATE TABLE external_bank_accounts
+(
+    id         BIGINT PRIMARY KEY,
+    name       TEXT                     NOT NULL,
+    logo_id    BIGINT                   REFERENCES file_attachments (id) ON DELETE SET NULL,
+    created_at timestamp with time zone NOT NULL,
+    updated_at timestamp with time zone NOT NULL
+);
+
+CREATE TABLE external_bank_account_ibans
+(
+    id                       BIGINT PRIMARY KEY,
+    external_bank_account_id BIGINT REFERENCES external_bank_accounts (id) ON DELETE CASCADE NOT NULL,
+    iban                     TEXT                                                            NOT NULL UNIQUE,
+    created_at               timestamp with time zone                                        NOT NULL,
+    updated_at               timestamp with time zone                                        NOT NULL
+);
+
+CREATE INDEX idx_external_bank_account_ibans_account_id ON external_bank_account_ibans (external_bank_account_id);
+CREATE INDEX idx_external_bank_account_ibans_iban ON external_bank_account_ibans (iban);
 
 -- ############################################################
 -- #                                                          #
@@ -127,6 +173,8 @@ CREATE TABLE bank_accounts
     updated_at             timestamp with time zone                 NOT NULL
 );
 
+CREATE INDEX idx_bank_accounts_iban ON bank_accounts (iban);
+
 -- ############################################################
 -- #                                                          #
 -- #                     Transactions                         #
@@ -135,11 +183,19 @@ CREATE TABLE bank_accounts
 
 CREATE TYPE transaction_type AS ENUM ('income', 'expense', 'transfer');
 
+CREATE TABLE transaction_parties
+(
+    id                       BIGINT PRIMARY KEY,
+    bank_account_id          BIGINT REFERENCES bank_accounts (id),
+    external_bank_account_id BIGINT REFERENCES external_bank_accounts (id),
+    created_at               timestamp with time zone NOT NULL,
+    updated_at               timestamp with time zone NOT NULL
+);
+
 CREATE TABLE linked_transactions
 (
     id            BIGINT PRIMARY KEY,
-    creditor_name TEXT,
-    creditor_iban TEXT,
+    creditor_iban TEXT REFERENCES external_bank_account_ibans (iban),
     created_at    timestamp with time zone NOT NULL,
     updated_at    timestamp with time zone NOT NULL
 );
@@ -147,9 +203,9 @@ CREATE TABLE linked_transactions
 CREATE TABLE transactions
 (
     id                    BIGINT PRIMARY KEY,
-    source                BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    destination           BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    currency              BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    source_id             BIGINT REFERENCES transaction_parties (id),
+    destination_id        BIGINT REFERENCES transaction_parties (id),
+    currency_id           BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     linked_transaction_id BIGINT                                                                REFERENCES linked_transactions (id) ON DELETE SET NULL,
     category_id           BIGINT                                                                REFERENCES categories (id) ON UPDATE SET NULL ON DELETE SET NULL,
     file_attachment_id    BIGINT                                                                REFERENCES file_attachments (id) ON DELETE SET NULL,
@@ -167,12 +223,16 @@ CREATE TABLE transactions
     updated_at            timestamp with time zone                                              NOT NULL
 );
 
+CREATE INDEX idx_transactions_source_id ON transactions (source_id);
+CREATE INDEX idx_transactions_destination_id ON transactions (destination_id);
+CREATE INDEX idx_transactions_category_id ON transactions (category_id);
+
 CREATE TABLE transaction_templates
 (
     id                    BIGINT PRIMARY KEY,
-    source                BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    destination           BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    currency              BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    source_id             BIGINT REFERENCES transaction_parties (id),
+    destination_id        BIGINT REFERENCES transaction_parties (id),
+    currency_id           BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     linked_transaction_id BIGINT                                                                REFERENCES linked_transactions (id) ON DELETE SET NULL,
     category_id           BIGINT                                                                REFERENCES categories (id) ON UPDATE SET NULL ON DELETE SET NULL,
     file_attachment_id    BIGINT                                                                REFERENCES file_attachments (id) ON DELETE SET NULL,
@@ -189,12 +249,16 @@ CREATE TABLE transaction_templates
     updated_at            timestamp with time zone                                              NOT NULL
 );
 
+CREATE INDEX idx_transaction_templates_source_id ON transaction_templates (source_id);
+CREATE INDEX idx_transaction_templates_destination_id ON transaction_templates (destination_id);
+CREATE INDEX idx_transaction_templates_category_id ON transaction_templates (category_id);
+
 CREATE TABLE recurring_transactions
 (
     id                    BIGINT PRIMARY KEY,
-    source                BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    destination           BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    currency              BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    source_id             BIGINT REFERENCES transaction_parties (id),
+    destination_id        BIGINT REFERENCES transaction_parties (id),
+    currency_id           BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     linked_transaction_id BIGINT                                                                REFERENCES linked_transactions (id) ON DELETE SET NULL,
     category_id           BIGINT                                                                REFERENCES categories (id) ON UPDATE SET NULL ON DELETE SET NULL,
     file_attachment_id    BIGINT                                                                REFERENCES file_attachments (id) ON DELETE SET NULL,
@@ -214,12 +278,16 @@ CREATE TABLE recurring_transactions
     updated_at            timestamp with time zone                                              NOT NULL
 );
 
+CREATE INDEX idx_recurring_transactions_source_id ON recurring_transactions (source_id);
+CREATE INDEX idx_recurring_transactions_destination_id ON recurring_transactions (destination_id);
+CREATE INDEX idx_recurring_transactions_category_id ON recurring_transactions (category_id);
+
 CREATE TABLE pending_transactions
 (
     id                    BIGINT PRIMARY KEY,
-    source                BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    destination           BIGINT REFERENCES bank_accounts (id) ON UPDATE CASCADE ON DELETE CASCADE,
-    currency              BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
+    source_id             BIGINT REFERENCES transaction_parties (id),
+    destination_id        BIGINT REFERENCES transaction_parties (id),
+    currency_id           BIGINT REFERENCES currencies (id) ON UPDATE CASCADE ON DELETE CASCADE NOT NULL,
     linked_transaction_id BIGINT                                                                REFERENCES linked_transactions (id) ON DELETE SET NULL,
     category_id           BIGINT                                                                REFERENCES categories (id) ON UPDATE SET NULL ON DELETE SET NULL,
     file_attachment_id    BIGINT                                                                REFERENCES file_attachments (id) ON DELETE SET NULL,
@@ -236,6 +304,10 @@ CREATE TABLE pending_transactions
     created_at            timestamp with time zone                                              NOT NULL,
     updated_at            timestamp with time zone                                              NOT NULL
 );
+
+CREATE INDEX idx_pending_transactions_source_id ON pending_transactions (source_id);
+CREATE INDEX idx_pending_transactions_destination_id ON pending_transactions (destination_id);
+CREATE INDEX idx_pending_transactions_category_id ON pending_transactions (category_id);
 
 -- ############################################################
 -- #                                                          #
@@ -254,6 +326,9 @@ CREATE TABLE contracts
     updated_at               timestamp with time zone                                        NOT NULL
 );
 
+CREATE INDEX idx_contracts_recurring_transaction_id ON contracts (recurring_transaction_id);
+CREATE INDEX idx_contracts_category_id ON contracts (category_id);
+
 CREATE TABLE inactive_contracts
 (
     id                  BIGINT PRIMARY KEY,
@@ -266,6 +341,78 @@ CREATE TABLE inactive_contracts
     updated_at          timestamp with time zone                              NOT NULL
 );
 
+CREATE INDEX idx_inactive_contracts_last_transaction_id ON inactive_contracts (last_transaction_id);
+CREATE INDEX idx_inactive_contracts_category_id ON inactive_contracts (category_id);
+
+
+-- ############################################################
+-- #                                                          #
+-- #                         Criteria                         #
+-- #                                                          #
+-- ############################################################
+
+CREATE TYPE filter_transaction_type AS ENUM ('all', 'contracts', 'non-contracts');
+
+CREATE TABLE budget_criteria
+(
+    id                         BIGINT PRIMARY KEY,
+    all_categories             BOOLEAN                  NOT NULL,
+    all_tags                   BOOLEAN                  NOT NULL,
+    all_external_bank_accounts BOOLEAN                  NOT NULL,
+    all_bank_accounts          BOOLEAN                  NOT NULL,
+    transaction_type           filter_transaction_type  NOT NULL,
+    created_at                 timestamp with time zone NOT NULL,
+    updated_at                 timestamp with time zone NOT NULL
+);
+
+CREATE TABLE budget_criteria_categories
+(
+    budget_criteria_id BIGINT REFERENCES budget_criteria (id) ON DELETE CASCADE,
+    category_id        BIGINT REFERENCES categories (id) ON DELETE CASCADE,
+    created_at         timestamp with time zone NOT NULL,
+    updated_at         timestamp with time zone NOT NULL,
+    PRIMARY KEY (budget_criteria_id, category_id)
+);
+
+CREATE INDEX idx_budget_criteria_categories_budget_criteria_id ON budget_criteria_categories (budget_criteria_id);
+CREATE INDEX idx_budget_criteria_categories_category_id ON budget_criteria_categories (category_id);
+
+CREATE TABLE budget_criteria_tags
+(
+    budget_criteria_id BIGINT REFERENCES budget_criteria (id) ON DELETE CASCADE,
+    tag_id             BIGINT REFERENCES tags (id) ON DELETE CASCADE,
+    created_at         timestamp with time zone NOT NULL,
+    updated_at         timestamp with time zone NOT NULL,
+    PRIMARY KEY (budget_criteria_id, tag_id)
+);
+
+CREATE INDEX idx_budget_criteria_tags_budget_criteria_id ON budget_criteria_tags (budget_criteria_id);
+CREATE INDEX idx_budget_criteria_tags_tag_id ON budget_criteria_tags (tag_id);
+
+CREATE TABLE budget_criteria_external_bank_accounts
+(
+    budget_criteria_id       BIGINT REFERENCES budget_criteria (id) ON DELETE CASCADE,
+    external_bank_account_id BIGINT REFERENCES external_bank_accounts (id) ON DELETE CASCADE,
+    created_at               timestamp with time zone NOT NULL,
+    updated_at               timestamp with time zone NOT NULL,
+    PRIMARY KEY (budget_criteria_id, external_bank_account_id)
+);
+
+CREATE INDEX idx_budget_criteria_external_bank_accounts_budget_criteria_id ON budget_criteria_external_bank_accounts (budget_criteria_id);
+CREATE INDEX idx_budget_criteria_external_bank_accounts_external_bank_account_id ON budget_criteria_external_bank_accounts (external_bank_account_id);
+
+CREATE TABLE budget_criteria_bank_accounts
+(
+    budget_criteria_id BIGINT REFERENCES budget_criteria (id) ON DELETE CASCADE,
+    bank_account_id    BIGINT REFERENCES bank_accounts (id) ON DELETE CASCADE,
+    created_at         timestamp with time zone NOT NULL,
+    updated_at         timestamp with time zone NOT NULL,
+    PRIMARY KEY (budget_criteria_id, bank_account_id)
+);
+
+CREATE INDEX idx_budget_criteria_bank_accounts_budget_criteria_id ON budget_criteria_bank_accounts (budget_criteria_id);
+CREATE INDEX idx_budget_criteria_bank_accounts_bank_account_id ON budget_criteria_bank_accounts (bank_account_id);
+
 -- ############################################################
 -- #                                                          #
 -- #                          Budget                          #
@@ -277,32 +424,19 @@ CREATE TYPE budget_type AS ENUM ('resetting', 'accumulating');
 CREATE TABLE budgets
 (
     id             BIGINT PRIMARY KEY,
-    type           budget_type              NOT NULL,
-    current_amount BIGINT                   NOT NULL,
-    amount         BIGINT                   NOT NULL,
-    cron           TEXT                     NOT NULL,
-    name           TEXT                     NOT NULL,
+    criteria_id    BIGINT REFERENCES budget_criteria (id) NOT NULL,
+    type           budget_type                            NOT NULL,
+    current_amount BIGINT                                 NOT NULL,
+    amount         BIGINT                                 NOT NULL,
+    cron           TEXT                                   NOT NULL,
+    name           TEXT                                   NOT NULL,
     description    TEXT,
-    map_all        BOOLEAN                  NOT NULL DEFAULT FALSE,
-    created_at     timestamp with time zone NOT NULL,
-    updated_at     timestamp with time zone NOT NULL
+    map_all        BOOLEAN                                NOT NULL DEFAULT FALSE,
+    created_at     timestamp with time zone               NOT NULL,
+    updated_at     timestamp with time zone               NOT NULL
 );
 
-CREATE TABLE budget_categories
-(
-    budget_id   BIGINT REFERENCES budgets (id) ON DELETE CASCADE,
-    category_id BIGINT REFERENCES categories (id) ON DELETE CASCADE,
-    PRIMARY KEY (budget_id, category_id)
-);
-
-CREATE TABLE budget_tags
-(
-    budget_id BIGINT REFERENCES budgets (id) ON DELETE CASCADE,
-    tag_id    BIGINT REFERENCES tags (id) ON DELETE CASCADE,
-    PRIMARY KEY (budget_id, tag_id)
-);
-
-CREATE TABLE budget_history
+CREATE TABLE budget_histories
 (
     id             BIGINT PRIMARY KEY,
     budget_id      BIGINT                   NOT NULL,
@@ -313,13 +447,9 @@ CREATE TABLE budget_history
     name           TEXT                     NOT NULL,
     description    TEXT,
     map_all        BOOLEAN                  NOT NULL DEFAULT FALSE,
-    recorded_at    timestamp with time zone NOT NULL DEFAULT now(),
+    recorded_at    timestamp with time zone NOT NULL,
     created_at     timestamp with time zone NOT NULL,
     updated_at     timestamp with time zone NOT NULL
 );
 
-/**
-  TODO:
-    - Add indexes
-    - Enhanced Budget Criteria
- */
+CREATE INDEX idx_budget_history_budget_id ON budget_histories (budget_id);
