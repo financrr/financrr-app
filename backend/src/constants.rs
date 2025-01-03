d4f8b3d9ef75be893a6b655de3bf775f3709afd9
Timon Klinkert <83671398+DenuxPlays@users.noreply.github.com>
2025-01-03 01:18:10 +0100

    Some minor backend and workflow fixes (#400)

--- backend/src/constants.rs
+++ backend/src/constants.rs
@@ -1,4 +1,11 @@
+use serde::Serialize;
+use utoipa::ToSchema;
+
 /// Api Versions
-pub const CURRENT_API_VERSION: &str = API_V1;
-pub const ALL_API_VERSIONS: [&str; 1] = [CURRENT_API_VERSION];
-pub const API_V1: &str = "v1";
+pub const CURRENT_API_VERSION: ApiVersions = ApiVersions::V1;
+pub const ALL_API_VERSIONS: [&ApiVersions; 1] = [&ApiVersions::V1];
+
+#[derive(Debug, Serialize, ToSchema)]
+pub enum ApiVersions {
+    V1,
+}
