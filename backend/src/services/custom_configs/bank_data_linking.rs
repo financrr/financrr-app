ae88f2d23175540419e2e7f2981fb83f7add81cc
Timon Klinkert <timon.klinkert@denux.dev>
2025-01-13 23:14:18 +0100

    idk what I've done

--- backend/src/services/custom_configs/bank_data_linking.rs
+++ backend/src/services/custom_configs/bank_data_linking.rs
@@ -12,6 +12,15 @@ pub enum LinkingImplementation {
     GoCardless(GoCardlessConfig),
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct GoCardlessConfig {
+    pub enabled: bool,
+    pub secret_id: String,
+    pub secret_key: String,
+    #[serde(default = "default_go_cardless_api_url")]
+    pub api_url: String,
+}
+
 impl LinkingImplementation {
     pub fn is_enabled(&self) -> bool {
         match self {
@@ -20,13 +29,10 @@ impl LinkingImplementation {
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct GoCardlessConfig {
-    pub enabled: bool,
-    pub secret_id: String,
-    pub secret_key: String,
-}
-
 fn default_enabled() -> bool {
     true
+}
+
+fn default_go_cardless_api_url() -> String {
+    "https://bankaccountdata.gocardless.com/".to_string()
 }
