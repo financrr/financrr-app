434b4230bf025a951b9b916d67e646d53da14eac
Denux <timon.klinkert@denux.dev>
2025-01-14 10:10:41 +0100

    only support gocardless (for now)
    - added some tests

--- backend/src/services/custom_configs/bank_data_linking.rs
+++ backend/src/services/custom_configs/bank_data_linking.rs
@@ -1,32 +1,13 @@
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 pub struct BankDataLinkingConfig {
     #[serde(default = "default_enabled")]
     pub enabled: bool,
-    pub implementations: Vec<LinkingImplementation>,
-}
-
-#[derive(Debug, Deserialize)]
-pub enum LinkingImplementation {
-    GoCardless(GoCardlessConfig),
-}
-
-#[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct GoCardlessConfig {
-    pub enabled: bool,
     pub secret_id: String,
     pub secret_key: String,
     #[serde(default = "default_go_cardless_api_url")]
     pub api_url: String,
-}
-
-impl LinkingImplementation {
-    pub fn is_enabled(&self) -> bool {
-        match self {
-            LinkingImplementation::GoCardless(conf) => conf.enabled,
-        }
-    }
 }
 
 fn default_enabled() -> bool {
